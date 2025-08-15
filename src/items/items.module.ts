176556d64e6d0f29a841701adc911fb933ed38fc
ControlMinero <omossol33@gmail.com>
2025-08-15 18:43:16 -0400

    Corrige nombres de archivos de entidades company e item

--- src/items/items.module.ts
+++ src/items/items.module.ts
@@ -3,9 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Item } from './entities/item.entity';
 import { ItemsService } from './items.service';
 import { ItemsController } from './items.controller';
+import { AuthModule } from '../auth/auth.module'; // Importamos el módulo de Auth
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Item])],
+  imports: [
+    TypeOrmModule.forFeature([Item]),
+    AuthModule, // <-- ¡Aquí está la corrección! Le damos acceso a la caja de herramientas.
+  ],
   controllers: [ItemsController],
   providers: [ItemsService],
 })
